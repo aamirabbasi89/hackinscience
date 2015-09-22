fbb315f4353a55e2a0d794536df1b591b245fb4c
Aamir Abbasi <aamir.abbasi@cri-paris.org>
2015-09-22 21:11:38 +0200

    my first exercise

--- exercises/320/solution.py
+++ exercises/320/solution.py
@@ -5,14 +5,14 @@ Created on Tue Sep 22 20:46:05 2015
 @author: Aamir Abbasi
 """
 import string
-letters = string.ascii_lowercase
-txtSize = 0
+letters = string.ascii_letters
 file = open('words.txt', 'r')
+txtSize = 0
 for alphabet in range(0, len(letters)):
     counter = 0
     for line in file:
+        txtSize = txtSize + len(line)
         for l in range(0, len(line)):
             if line[l] == letters[alphabet]:
                 counter = counter + 1
-        txtSize = txtSize + len(line)
     print(letters[alphabet], ':', counter/txtSize)
---
+++ exercises/328/solution.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 22 21:07:11 2015
+
+@author: Aamir Abbasi
+"""
+
+
+def mul(alist):
+    for i in range(0, len(alist) - 1):
+        multiple = alist[i] * alist [i + 1]
+    print(multiple)
