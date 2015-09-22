bbe50216146c09660fa275c2bba1a72445a4f8f2
Aamir Abbasi <aamir.abbasi@cri-paris.org>
2015-09-22 18:53:30 +0200

    my first exercise

---
+++ exercises/240/solution.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 22 18:46:00 2015
+
+@author: Aamir Abbasi
+"""
+n1 = 1
+n2 = 2
+alist= [n1, n2]
+for i in range(0, 8):
+    n3 = n1 + n2
+    alist.append(n3)
+    n1 = n2
+    n2 = n3
+print(alist, '.')
