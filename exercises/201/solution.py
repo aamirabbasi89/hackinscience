7198dce1d4823ab19c04bf84b0211ff9cbd450de
Aamir Abbasi <aamir.abbasi@cri-paris.org>
2015-09-22 17:15:59 +0200

    my first exercise

--- exercises/201/solution.py
+++ exercises/201/solution.py
@@ -6,9 +6,14 @@ Created on Tue Sep 22 16:50:47 2015
 """
 
 
-def is_alpha(input):
-    for i in range(0, len(input)):
-        if input[i].isalpha():
-            return True
-        else:
-            return False
+def is_alpha(input):   
+    import string
+    letters = string.ascii_letters
+    for i in range(0, len(letters)):
+        for j in range(0, len(input)):
+            if input(j) != letters(i):
+                flag = 1
+    if flag == 1:
+        return False
+    else:
+        return True
