bd98c954cc864ced4e408074d0c96fa8ed61a318
Aamir Abbasi <aamir.abbasi@cri-paris.org>
2015-09-22 14:46:53 +0200

    my first exercise

--- exercises/020/solution.py
+++ exercises/020/solution.py
@@ -7,6 +7,6 @@ Created on Mon Sep 21 17:05:14 2015
 """
 import sys
 if (len(sys.argv) < 2):
-    print("usage: python3 solution.py PARAM.")
+    print("usage: python3 solution.py OP1 OP2")
 else:
     print(str(int(sys.argv[1]) - int(sys.argv[2])))
