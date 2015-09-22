bf580b25394059d59a4a8f8b1e921ff7fd2012f1
Aamir Abbasi <aamir.abbasi@cri-paris.org>
2015-09-22 15:39:55 +0200

    my first exercise

--- exercises/014/solution.py
+++ exercises/014/solution.py
@@ -7,6 +7,6 @@ Created on Mon Sep 21 16:33:35 2015
 """
 import sys
 if (len(sys.argv) < 2):
-    print("usage: python3 solution.py PARAM.")
+    print('usage: python3 solution.py PARAM')
 else:
     print(sys.argv[1])
---
+++ exercises/107/solution.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Exercise 107
+Created on Tue Sep 22 15:14:01 2015
+
+@author: Aamir Abbasi
+"""
+def select_student(my_class, m):
+    
+    
+    for i in range(0, len(my_class)):
+        if my_class[i][2] < m:
+            result= {'Accepted': my_class[i]}
+        else:
+            result= {'Rejected': my_class[i]}
+    print(result)
