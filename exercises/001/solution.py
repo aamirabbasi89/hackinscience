ce5624a4516b6aa9019d68ffaf85e29e872a87aa
Aamir Abbasi <aamir.abbasi@cri-paris.org>
2015-09-22 09:32:30 +0200

    my first exercise

--- exercises/001/solution.py
+++ exercises/001/solution.py
@@ -8,23 +8,23 @@ Created on Mon Sep 21 11:03:39 2015
 
 '"Isn\'t", she said.'
 print('"Isn\'t", she said.')
-s= 'First line. \nSecond line.' # \n means new line
+s = 'First line. \nSecond line.'  # \n means new line
 print(s)
 
-print ('C:\some\name')
-print (r'C:\some\name')
+print('C:\some\name')
+print(r'C:\some\name')
 
-print ("""\ 
+print("""\ 
 Usage: thingy[OPTIONS]
        -h                 Display this usage message
        -H                 Hostname to connect to 
 """)
 
-string= 3* 'un' + 'ium'
+string = 3 * 'un' + 'ium'
 print(string);
 
 print('Py' 'thon')
-text= ('This' ' ' 'is' ' ' 'a' ' ' 'lovely' ' ' 'morning')
+text = ('This' ' ' 'is' ' ' 'a' ' ' 'lovely' ' ' 'morning')
 print(text)
 
 word = 'Paris'
@@ -33,12 +33,12 @@ print('first character' ' ', word[0])
 print('third character' ' ', word[3])
 print('last character' ' ', word[4])
 
-print('last character' ' ',word[-1]) # last character
-print('second last character' ' ',word[-2]) # second last character
+print('last character' ' ',word[-1])  # last character
+print('second last character' ' ',word[-2])  # second last character
 
 print(word[0:3])
 
 print(word + ' ' "Je t'aime")
 
-newString= word + ' ' "Je t'aime"
+newString = word + ' ' "Je t'aime"
 print(len(newString))
