ddc692955f0251e7a2a30b79ef59f293689911a1
Aamir Abbasi <aamir.abbasi@cri-paris.org>
2015-09-22 14:30:42 +0200

    my first exercise

--- exercises/014/solution.py
+++ exercises/014/solution.py
@@ -6,7 +6,7 @@ Created on Mon Sep 21 16:33:35 2015
 @author: Aamir Abbasi
 """
 import sys
-if (len(sys.argv)<2):
+if (len(sys.argv) < 2):
     print("usage: python3 solution.py PARAM.")
 else:
     print(sys.argv[1])
