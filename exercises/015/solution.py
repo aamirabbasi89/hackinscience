909cb7e12404f3a634fcdb4d490c4312463aeab4
Aamir Abbasi <aamir.abbasi@cri-paris.org>
2015-09-22 14:37:24 +0200

    my first exercise

--- exercises/015/solution.py
+++ exercises/015/solution.py
@@ -6,5 +6,5 @@ Created on Mon Sep 21 13:50:32 2015
 @author: Aamir Abbasi
 """
 import math
-print(245850922/78256779)
-print((1+ math.pow(5,0.5))/2)
+print(245850922 / 78256779)
+print((1 + math.pow(5, 0.5)) / 2)
