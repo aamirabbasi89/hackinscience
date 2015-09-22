9cf5d31e11cbcace4d5b5550d9493c4a722669b0
Aamir Abbasi <aamir.abbasi@cri-paris.org>
2015-09-22 15:57:03 +0200

    my first exercise

--- exercises/110/solution.py
+++ exercises/110/solution.py
@@ -6,7 +6,7 @@ Created on Tue Sep 22 15:29:58 2015
 @author: Aamir Abbasi
 """
 import sys
-if (len(sys.argv) < 4:
+if (len(sys.argv)) < 4:
     print("usage: ./solution.py a_number (an_operator +-*/%^) a_number")
 else:
     output = sys.argv[1] (sys.argv[2]) sys.argv[3]
---
+++ exercises/solution.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 22 15:50:24 2015
+
+@author: Aamir Abbasi
+"""
+def is_prime(num):
+    
+    
+    if num % 2 == 0 & num != 2:
+        return False
+    else:
+        return True
