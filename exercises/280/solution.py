b90ca28bfac83f25c9f2845d4c427ac32d826f74
Aamir Abbasi <aamir.abbasi@cri-paris.org>
2015-09-22 19:08:41 +0200

    my first exercise

---
+++ exercises/280/solution.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Exercise 280
+Created on Tue Sep 22 19:02:11 2015
+
+@author: Aamir Abbasi
+"""
+import sys
+try:
+    print(sys.argv[1])
+except:
+    print("Not Enough Parameters")
