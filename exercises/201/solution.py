92bfe311922ec31d5eb3a33c34f58bc65f20f25e
Aamir Abbasi <aamir.abbasi@cri-paris.org>
2015-09-22 16:57:04 +0200

    my first exercise

---
+++ exercises/201/solution.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 22 16:50:47 2015
+
+@author: Aamir Abbasi
+"""
+
+
+def is_alpha(input):
+    for i in range(0, len(input)):
+        if i.isalpha():
+            return True
+        else:
+            return False
+    
