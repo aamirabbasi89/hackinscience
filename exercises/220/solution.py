8241fc341ee3a5175920f1315f4a995c9f97a3e9
Aamir Abbasi <aamir.abbasi@cri-paris.org>
2015-09-22 17:42:49 +0200

    my first exercise

---
+++ exercises/220/solution.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Exercise 220
+Created on Tue Sep 22 17:26:28 2015
+
+@author: Aamir Abbasi
+"""
+alist=[];
+for i in range(10000, 10050):
+        if i % 2 != 0 & i % 3 != 0 & i % 5 != 0:
+            alist.append(i)
+print(alist)
