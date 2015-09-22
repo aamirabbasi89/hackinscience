c896ef4c50b693dacd53e0179559a9cef29b5784
Aamir Abbasi <aamir.abbasi@cri-paris.org>
2015-09-22 12:58:30 +0200

    my first exercises

--- exercises/001/solution.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-For Exercise on STRINGS
-Created on Mon Sep 21 11:03:39 2015
-
-@author: Aamir Abbasi
-"""
--- exercises/008/solution.py
+++ exercises/008/solution.py
@@ -6,8 +6,7 @@ Created on Mon Sep 21 13:37:07 2015
 
 @author: Aamir Abbasi
 """
-
-list= 'Hello world!'
-for i in range(0,len(list)):
+list = 'Hello world!'
+for i in range(0, len(list)):
     print(list[i])
     
