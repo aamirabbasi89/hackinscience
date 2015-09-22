530a177c122c8f3fbbc9bf2a8810f1b766eb3935
Aamir Abbasi <aamir.abbasi@cri-paris.org>
2015-09-22 21:13:35 +0200

    my first exercise

--- exercises/328/solution.py
+++ exercises/328/solution.py
@@ -8,5 +8,5 @@ Created on Tue Sep 22 21:07:11 2015
 
 def mul(alist):
     for i in range(0, len(alist) - 1):
-        multiple = alist [i] * alist [i + 1]
+        multiple = alist[i] * alist[i + 1]
     print(multiple)
