941f5d39b9d0d03e25ff5aa08a6eca340c5b6232
Aamir Abbasi <aamir.abbasi@cri-paris.org>
2015-09-23 13:26:49 +0200

    my first exercise

---
+++ exercises/202/solution.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 23 13:19:47 2015
+
+@author: Aamir Abbasi
+"""
+
+
+def starts_with(input, value):
+    if input.startswith(value):
+        return True
+    else:
+        return False
