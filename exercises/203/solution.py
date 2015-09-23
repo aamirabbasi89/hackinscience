8b8b19430fbba7b98cee11aab08080588169dd69
Aamir Abbasi <aamir.abbasi@cri-paris.org>
2015-09-23 13:00:16 +0200

    my first exercise

---
+++ exercises/203/solution.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Exercise 203
+Created on Wed Sep 23 12:57:04 2015
+
+@author: Aamir Abbasi
+"""
+
+
+def is_multiple(a, b):
+    if b % 2 == 0:
+        return True
+    else:
+        return False
