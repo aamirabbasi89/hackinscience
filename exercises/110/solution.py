e1a8d3072723b201870317c6b5782fed89853aa5
Aamir Abbasi <aamir.abbasi@cri-paris.org>
2015-09-24 12:33:48 +0200

    my first exercises

--- exercises/110/solution.py
+++ exercises/110/solution.py
@@ -6,7 +6,7 @@ Created on Tue Sep 22 15:29:58 2015
 @author: Aamir Abbasi
 """
 import sys
-if (len(sys.argv)) < 4 or sys.argv[2] != '+':
+if (len(sys.argv)) < 4:
     print("usage: ./solution.py a_number (an_operator +-*/%^) a_number")
 else:
     if sys.argv[2] == '+':
@@ -21,3 +21,15 @@ else:
         print(int(sys.argv[1]) % int(sys.argv[3]))
     if sys.argv[2] == '^':
         print(int(sys.argv[1]) ** int(sys.argv[3]))
+    if sys.argv[2] != '+':
+        print("usage: ./solution.py a_number (an_operator +-*/%^) a_number")
+    if sys.argv[2] != '-':
+        print("usage: ./solution.py a_number (an_operator +-*/%^) a_number")
+    if sys.argv[2] != '*':
+        print("usage: ./solution.py a_number (an_operator +-*/%^) a_number")
+    if sys.argv[2] != '/':
+        print("usage: ./solution.py a_number (an_operator +-*/%^) a_number")
+    if sys.argv[2] != '%':
+        print("usage: ./solution.py a_number (an_operator +-*/%^) a_number")
+    if sys.argv[2] != '^':
+        print("usage: ./solution.py a_number (an_operator +-*/%^) a_number")
