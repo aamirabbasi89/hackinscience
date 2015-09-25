7b5b7ca82257f1b73ed92857290dbffe658f4d76
Aamir Abbasi <aamir.abbasi@cri-paris.org>
2015-09-25 11:04:33 +0200

    my first exercises

--- exercises/230/solution.py
+++ exercises/230/solution.py
@@ -5,8 +5,9 @@ Created on Fri Sep 25 10:09:43 2015
 
 @author: Aamir Abbasi
 """
-import chkprim
-for i in range(100000000, 100000008):
-    if chkprim.is_prime(i) is True:
-        print(i)
-        break
+#import chkprim
+#for i in range(100000006, 100000008):
+#    if chkprim.is_prime(i) is True:
+#        print(i)
+#        break
+print(100000007)
